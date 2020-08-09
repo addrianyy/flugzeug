fd7df7b4b6344c312bd10612a9bf239dd60385b0
addrianyy <adrianvpl@gmail.com>
2020-08-09 22:44:51 +0200

    Add E820 memory map.

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -59,20 +59,63 @@ impl core::fmt::Write for SerialPort {
 }
 
 #[no_mangle]
-extern "C" fn _start(boot_disk_descriptor: u32, boot_disk_data: u32) -> ! {
-    unsafe { core::ptr::write(0xb8000 as *mut u16, 0x4343); }
-
+extern "C" fn _start(_boot_disk_descriptor: u32, _boot_disk_data: u32) -> ! {
     let mut serial = unsafe { SerialPort::new() };
-    let _ = write!(serial, "{:X} {:x}", boot_disk_descriptor, boot_disk_data);
 
-    unsafe {
-        bios::interrupt(0x10, &mut bios::RegisterState {
-            eax: 0x0003,
+    let mut sequence = 0;
+
+    loop {
+        #[repr(C)]
+        #[derive(Default, Debug)]
+        struct E820Entry {
+            base: u64,
+            size: u64,
+            typ:  u32,
+            acpi: u32,
+        }
+
+        // Some BIOSes won't set ACPI field so we need to make it valid in the beginning.
+        let mut entry = E820Entry {
+            acpi: 1,
             ..Default::default()
-        });
+        };
+
+        // Make sure that the entry is accessible by BIOS.
+        assert!((&entry as *const _ as usize) < 0x10000,
+                "Entry is in high memory, BIOS won't be able to access it.");
+
+        // Make sure that size matches excpected one.
+        assert!(core::mem::size_of::<E820Entry>() == 24, "E820 entry has invalid size.");
+
+        // Load all required magic values for this BIOS service.
+        let mut regs = bios::RegisterState {
+            eax: 0xe820,
+            ebx: sequence,
+            ecx: core::mem::size_of::<E820Entry>() as u32,
+            edx: u32::from_be_bytes(*b"SMAP"),
+            edi: &mut entry as *mut _ as u32,
+            ..Default::default()
+        };
+
+        unsafe { bios::interrupt(0x15, &mut regs); }
+
+        // Update current sequence so BIOS will know which entry to report in the next iteration.
+        sequence = regs.ebx;
+
+        // Don't do anything with this entry if ACPI bit 0 is not set. It should be skipped.
+        // If BIOS didn't set it we assume it's valid.
+        if entry.acpi & 1 != 0 {
+            let _ = write!(serial, "{:x?}\n", entry);
+        }
+
+        // CF set indicates error or end of the list, stop iteration. sequence == 0 indicates
+        // end of the list.
+        if regs.eflags & 1 != 0 || sequence == 0 {
+            break;
+        }
     }
 
-    unsafe { core::ptr::write(0xb8000 as *mut u16, 0x4141); }
+    let _ = write!(serial, "Done!\n");
 
     loop {}
 }
